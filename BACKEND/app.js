767a7df3eca71dbdeec90d20e16a68622a0fc4d4
Harsh <harshagarwal81528@gmail.com>
2025-05-18 18:36:31 +0530

    Implement URL shortening feature with MongoDB integration and add necessary dependencies

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,33 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { nanoid } from 'nanoid';
+import connectDB from './src/config/mongo.config.js';
+import urlSchema from './src/models/shortUrlSchema.js';
+
+dotenv.config();
 
 const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/create", (req, res) => {
+  const { originalUrl } = req.body;
+  if (!originalUrl) {
+    return res.status(400).json({ error: "Original URL is required" });
+  }
+
+  const shortUrl = nanoid(10);
+  
+
+  const newUrl = new urlSchema({
+    originalUrl,
+    shortUrl,
+  });
+  newUrl.save()
+  res.send({ originalUrl, shortUrl });
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ BACKEND/src/models/shortUrlSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+export const Url = mongoose.model("Url", urlSchema);
+export default Url;
+ 
