cab6c77749e56506542ddc0e5d061b569b96d226
Harsh <harshagarwal81528@gmail.com>
2025-05-23 14:34:21 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created App component as the main application entry point.
    - Added global CSS file with Tailwind CSS import.
    - Set up main entry point for React application rendering.
    - Configured Vite with React and Tailwind CSS plugins.

--- BACKEND/src/controllers/shortUrl.controllers.js
+++ BACKEND/src/controllers/shortUrl.controllers.js
@@ -1,18 +1,19 @@
 import * as shortUrlService from '../services/shortUrl.services.js';
+import wrapAsync from '../utils/tryCatchWrapper.js';
 
 
-export const createShortUrl = async (req, res) => {
+export const createShortUrl = wrapAsync(async (req, res) => {
   const { originalUrl } = req.body;
   const shortUrl = await shortUrlService.createShortUrl(originalUrl);
   res.status(201).json({ shortUrl: process.env.APP_URL + shortUrl });
  
-};
+});
 
-export const redirectFromShortUrl = async (req, res) => {
+export const redirectFromShortUrl = wrapAsync(async (req, res) => {
   const { shortUrl } = req.params;
   const url = await shortUrlService.getUrl(shortUrl);
   if(!url) throw new Error("URL not found");
   url.clicks += 1;
   await url.save();
   res.redirect(url.originalUrl);
-};
+});
